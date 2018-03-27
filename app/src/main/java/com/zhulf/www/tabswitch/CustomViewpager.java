00e02f48fe41ead1c56bbc9f2dd08d8047576b3e
andevele <andevele@163.com>
2018-03-28 00:01:40 +0800

    Initial commit

---
+++ app/src/main/java/com/zhulf/www/tabswitch/CustomViewpager.java
@@ -0,0 +1,19 @@
+package com.zhulf.www.tabswitch;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+/**
+ * Created by Administrator on 2018/3/27.
+ */
+
+public class CustomViewpager extends ViewPager {
+    public CustomViewpager(Context context) {
+        super(context);
+    }
+
+    public CustomViewpager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
